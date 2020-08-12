1a1161380b3b8b2b112ff23b49ea3913b669cc72
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-11 23:47:30 -0500

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 $('#js-shopping-list-form').on('submit', function(event) {
         let text = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li>' + text + '</li>');
+        $('.shopping-list').append('<li> <span class="shopping-item">' + text + '</span> </li>');
         event.preventDefault(); 
 });    
 
