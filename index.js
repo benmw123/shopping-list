fd80ba01d4e04e5fc58f86236dfb0afdb75f488d
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-11 23:36:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+$('#js-shopping-list-form').on('submit', function(event) {
+        let text = $('#shopping-list-entry').val();
+        $('.shopping-list').append('<li>' + text + '</li>');
+        event.preventDefault(); 
+});    
+
+    
+    
+
+
