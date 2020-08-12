5422712be33b6af71b9f99f485618ca50fcdce35
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-12 00:00:11 -0500

    adds new item with all styling

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 $('#js-shopping-list-form').on('submit', function(event) {
         let text = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li> <span class="shopping-item">' + text + '</span> </li>');
+        $('.shopping-list').append(
+            '<li>'
+            +'<span class="shopping-item">'+text+'</span>'
+            +'<div class="shopping-item-controls">'
+            +'<button class="shopping-item-toggle">'
+            +'<span class="button-label">check</span>'
+            +'</button>'
+            +'<button class="shopping-item-delete">'
+            +'<span class="button-label">delete</span>'
+            +'</button>'
+            +'</div>'
+            +'</li>');
         event.preventDefault(); 
 });    
 
