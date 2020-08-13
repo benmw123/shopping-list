b16f23c24306e2d41f74bde957cefad1c1378aa3
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-13 11:38:06 -0500

    ability to remove working with og li's

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ $('#js-shopping-list-form').on('submit', function(event) {
         $('.shopping-list').append(
             '<li>'
             +'<span class="shopping-item">'+text+'</span>'
-            +'<div class="shopping-item-controls">'
-            +'<button class="shopping-item-toggle">'
-            +'<span class="button-label">check</span>'
-            +'</button>'
-            +'<button class="shopping-item-delete">'
-            +'<span class="button-label">delete</span>'
-            +'</button>'
-            +'</div>'
+                +'<div class="shopping-item-controls">'
+                    +'<button class="shopping-item-toggle">'
+                    +'<span class="button-label">check</span>'
+                    +'</button>'
+                    +'<button class="shopping-item-delete">'
+                    +'<span class="button-label">delete</span>'
+                    +'</button>'
+                +'</div>'
             +'</li>');
         //resets shopping-list-entry after each sumbit.        
         $('#shopping-list-entry').val();
@@ -24,11 +24,14 @@ $('#js-shopping-list-form').on('submit', function(event) {
 
 //need to figure out how to only select this.grandparent. 
 $('.shopping-item-toggle').click(function() {
-    $(this).parents('li').toggleClass('shopping-item__checked'); 
+    $(this).closest('span.shopping-item').toggleClass('shopping-item__checked'); 
     
 });   
 
 //i also need to fix the padding on the appending shopping list item controls 
     
-
+//delete an entry
+$('.shopping-item-delete').click(function () {
+    $(this).closest('li').remove(); 
+});
 
