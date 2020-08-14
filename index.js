9dc26a2ab4e53a04edbea1287949c9c1821c830d
Benjamin Whitehurst <61037364+benmw123@users.noreply.github.com>
2020-08-13 23:51:52 -0500

    got strike through to work

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //select the shopping form, when submited run function(event) to ammend shopping list. 
-$('#js-shopping-list-form').on('submit', function(event) {
+$('document').ready(function() {
+
+    $('#js-shopping-list-form').on('submit', function(event) {
         //store dom element as text to append to list.
         let text = $('#shopping-list-entry').val();
-        //apend the ul .shopping list with the necessary html using concatenation to add text.
-        $('.shopping-list').append(
-            '<li>'
-            +'<span class="shopping-item">'+text+'</span>'
-                +'<div class="shopping-item-controls">'
+            //apend the ul .shopping list with the necessary html using concatenation to add text.
+            $('.shopping-list').append(
+                '<li>'
+                +'<span class="shopping-item">'+text+'</span>'
+                    +'<div class="shopping-item-controls">'
                     +'<button class="shopping-item-toggle">'
                     +'<span class="button-label">check</span>'
                     +'</button>'
@@ -15,23 +17,22 @@ $('#js-shopping-list-form').on('submit', function(event) {
                     +'</button>'
                 +'</div>'
             +'</li>');
-        //resets shopping-list-entry after each sumbit.        
-        $('#shopping-list-entry').val();
-        $('#shopping-list-entry').val('');
-        //prevents default form behavior. 
-        event.preventDefault(); 
-}); 
+            //resets shopping-list-entry after each sumbit.        
+            $('#shopping-list-entry').val();
+            $('#shopping-list-entry').val('');
+            //prevents default form behavior. 
+            event.preventDefault(); 
+    }); 
 
-//need to figure out how to only select this.grandparent. 
-$('.shopping-item-toggle').click(function() {
-    $(this).closest('span.shopping-item').toggleClass('shopping-item__checked'); 
-    
-});   
+    $('.shopping-item-delete').on('click', function () {
+        $(this).closest('li').remove(); 
+    });
 
-//i also need to fix the padding on the appending shopping list item controls 
+    $('.shopping-item-toggle').on('click', function () {
+        $(this).parents('li').children('span').toggleClass('shopping-item__checked');    
+    });   
     
-//delete an entry
-$('.shopping-item-delete').click(function () {
-    $(this).closest('li').remove(); 
 });
+
+
 
